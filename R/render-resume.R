64ce05b34c474cd9b59dcf6eb924221fbd65e31b
Jordan <52896238+Jordan-Krogmann@users.noreply.github.com>
2022-03-28 13:49:04 -0400

    updated resume

--- R/render-resume.R
+++ R/render-resume.R
@@ -3,4 +3,7 @@
 
 
 
-rmarkdown::render("analysis.R", "pdf_document")
+pagedown::chrome_print(
+  input = here::here("markdown", "resume_example.Rmd"),
+  output = here::here("documents", paste0(Sys.Date(), "_jordan-resume.pdf"))
+)
